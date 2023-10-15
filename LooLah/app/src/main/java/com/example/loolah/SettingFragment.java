3b606597c5630a907a731b19899338201463f6fa
Chloie Tan <chloie.tan.ct@gmail.com>
2023-10-15 18:34:15 +0800

    Add profile settings page

--- LooLah/app/src/main/java/com/example/loolah/SettingFragment.java
+++ LooLah/app/src/main/java/com/example/loolah/SettingFragment.java
@@ -5,23 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-public class ProfileFragment extends Fragment {
+public class SettingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View profile_fragment = inflater.inflate(R.layout.fragment_profile, container, false);
+        View setting_fragment = inflater.inflate(R.layout.fragment_setting, container, false);
 
-        ImageButton btn_setting = profile_fragment.findViewById(R.id.ib_profile_settings);
-        btn_setting.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_settingFragment);
+        Button btn_sign_out = setting_fragment.findViewById(R.id.btn_setting_sign_out);
+        btn_sign_out.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+            getActivity().overridePendingTransition(0, 0);
         });
 
-        return profile_fragment;
+        return setting_fragment;
     }
 }
