fa974dbb687176969a835bc050bedbb906aecf8f
Chloie Tan <chloie.tan.ct@gmail.com>
2023-10-16 01:13:10 +0800

    Add change password page

--- LooLah/app/src/main/java/com/example/loolah/ChangePasswordFragment.java
+++ LooLah/app/src/main/java/com/example/loolah/ChangePasswordFragment.java
@@ -14,27 +14,21 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SettingFragment extends Fragment {
+public class ChangePasswordFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View setting_fragment = inflater.inflate(R.layout.fragment_setting, container, false);
+        View setting_fragment = inflater.inflate(R.layout.fragment_change_password, container, false);
 
-        ImageButton btn_back = setting_fragment.findViewById(R.id.ib_setting_back);
-        Button btn_change_password = setting_fragment.findViewById(R.id.btn_setting_change_password);
-        Button btn_sign_out = setting_fragment.findViewById(R.id.btn_setting_sign_out);
+        Button btn_save = setting_fragment.findViewById(R.id.btn_change_password_save);
+        ImageButton btn_back = setting_fragment.findViewById(R.id.ib_change_password_back);
+
+        btn_save.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_changePasswordFragment_to_profileFragment);
+        });
 
         btn_back.setOnClickListener(v -> {
             NavHostFragment navHostFragment = (NavHostFragment) getParentFragment();
             navHostFragment.getNavController().navigateUp();
-        });
-
-        btn_change_password.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_settingFragment_to_changePasswordFragment);
-        });
-
-        btn_sign_out.setOnClickListener(v -> {
-            startActivity(new Intent(getActivity(), LoginActivity.class));
-            getActivity().overridePendingTransition(0, 0);
         });
 
         return setting_fragment;
