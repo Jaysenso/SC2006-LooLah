1d87d6965f4b19f4cefa4827b524938956a344a5
Deuel Teo <deuel.teo.2000@gmail.com>
2023-10-24 19:50:51 +0800

    added fav fragment, edited toilet details to be able to favorite, edited favfragment to navigation

---
+++ LooLah/app/src/main/java/com/example/loolah/FavoriteFragment.java
@@ -0,0 +1,42 @@
+package com.example.loolah;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class FavoriteFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View fav_fragment = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        String[] toilet_types = new String[]{"Type", "Bus Interchange", "Club", "Coffeeshop", "Foodcourt", "Government Office", "Market & Food Centre", "MRT Station", "Park", "Pier", "Place of worship", "Private Office", "Restaurant", "Shopping Centre", "Tourist Attraction", "Community Centre", "Food Court", "Dormitory", "Industrial Complex"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.requireContext(), R.layout.item_spinner, toilet_types);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) fav_fragment.findViewById(R.id.sp_fav_filter_type)).setAdapter(adapter);
+
+        String[] toilet_districts = new String[]{"District", "Central", "North East", "North West", "South East", "South West"};
+        adapter = new ArrayAdapter<>(this.requireContext(), R.layout.item_spinner, toilet_districts);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) fav_fragment.findViewById(R.id.sp_fav_filter_district)).setAdapter(adapter);
+
+        String[] toilet_rating = new String[]{"Rating", "1 star", "2 star", "3 star", "4 star", "5 star"};
+        adapter = new ArrayAdapter<>(this.requireContext(), R.layout.item_spinner, toilet_rating);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) fav_fragment.findViewById(R.id.sp_fav_filter_rating)).setAdapter(adapter);
+
+        LinearLayout row_toilet = fav_fragment.findViewById(R.id.lnl_fav_toilet);
+        row_toilet.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_favoritesFragment_to_toiletDetailsFragment);
+        });
+
+        return fav_fragment;
+    }
+}
--- LooLah/app/src/main/java/com/example/loolah/ToiletDetailsFragment.java
+++ LooLah/app/src/main/java/com/example/loolah/ToiletDetailsFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class ToiletDetailsFragment extends Fragment {
+    boolean isPlay = false;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View toilet_details_fragment = inflater.inflate(R.layout.fragment_toilet_details, container, false);
@@ -21,6 +22,17 @@ public class ToiletDetailsFragment extends Fragment {
         btn_back.setOnClickListener(v -> {
             NavHostFragment navHostFragment = (NavHostFragment) getParentFragment();
             navHostFragment.getNavController().navigateUp();
+        });
+
+        ImageButton btn_fav = toilet_details_fragment.findViewById(R.id.ib_toilet_details_favorite);
+
+        btn_fav.setOnClickListener(v -> {
+            if(isPlay){
+                btn_fav.setImageResource(R.drawable.ic_toilet_details_favorite);//sets button to default
+            }else{
+                btn_fav.setImageResource(R.drawable.ic_toilet_details_favorited);//sets button to favorited
+            }
+            isPlay = !isPlay; // reverse
         });
 
         return toilet_details_fragment;
