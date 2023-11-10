d5505d4dad6e8e8cf1a35e06c2444bc56ed01374
DeuelT <102650797+DeuelT@users.noreply.github.com>
2023-11-10 22:49:48 +0800

    fav fragment edit

--- LooLah/app/src/main/java/com/example/loolah/view/favorites/FavoriteFragment.java
+++ LooLah/app/src/main/java/com/example/loolah/view/favorites/FavoriteFragment.java
@@ -1,41 +1,84 @@
-package com.example.loolah.view.Favorites;
+package com.example.loolah.view.favorites;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.loolah.R;
+import com.example.loolah.adapter.FavoriteAdapter;
+import com.example.loolah.databinding.FragmentFavoriteBinding;
+import com.example.loolah.model.Toilet;
+import com.example.loolah.viewmodel.FavoriteViewModel;
 
-public class FavoriteFragment extends Fragment {
+import java.util.ArrayList;
+
+public class FavoriteFragment extends Fragment implements FavoriteAdapter.OnItemClickListener {
+    private FavoriteViewModel viewModel;
+    private FragmentFavoriteBinding binding;
+    private FavoriteAdapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View favorite_fragment = inflater.inflate(R.layout.fragment_favorite, container, false);
+        viewModel = new ViewModelProvider(getActivity()).get(FavoriteViewModel.class);
+
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false);
+        binding.setLifecycleOwner(getActivity());
+        binding.setFavoriteView(this);
+
+        adapter = new FavoriteAdapter();
+        adapter.setOnItemClickListener(this);
+        binding.rvFavToilets.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        binding.rvFavToilets.setAdapter(adapter);
+
+        viewModel.getFavoritesToiletListMutableLiveData().observe(getViewLifecycleOwner(), toilets -> {
+            switch (toilets.getStatus()) {
+                case SUCCESS:
+                    ArrayList<Toilet> toiletList = toilets.getData();
+                    if (toiletList.size() == 0) binding.tvFavNoToilets.setVisibility(View.VISIBLE);
+                    else binding.tvFavNoToilets.setVisibility(View.GONE);
+                    adapter.setFavoritesToiletList(toiletList);
+                    break;
+                case ERROR:
+                    Log.d("TEST", "error");
+                    break;
+                case LOADING:
+                    break;
+            }
+        });
+        viewModel.getFavoriteToilets();
 
         String[] toilet_types = new String[]{"Type", "Bus Interchange", "Club", "Coffeeshop", "Foodcourt", "Government Office", "Market & Food Centre", "MRT Station", "Park", "Pier", "Place of worship", "Private Office", "Restaurant", "Shopping Centre", "Tourist Attraction", "Community Centre", "Food Court", "Dormitory", "Industrial Complex"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this.requireContext(), R.layout.item_spinner, toilet_types);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ((Spinner) favorite_fragment.findViewById(R.id.sp_favorite_filter_type)).setAdapter(adapter);
+        binding.spFavoriteFilterType.setAdapter(adapter);
 
         String[] toilet_districts = new String[]{"District", "Central", "North East", "North West", "South East", "South West"};
         adapter = new ArrayAdapter<>(this.requireContext(), R.layout.item_spinner, toilet_districts);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ((Spinner) favorite_fragment.findViewById(R.id.sp_favorite_filter_district)).setAdapter(adapter);
+        binding.spFavoriteFilterDistrict.setAdapter(adapter);
 
         String[] toilet_rating = new String[]{"Rating", "1 star", "2 star", "3 star", "4 star", "5 star"};
         adapter = new ArrayAdapter<>(this.requireContext(), R.layout.item_spinner, toilet_rating);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ((Spinner) favorite_fragment.findViewById(R.id.sp_favorite_filter_rating)).setAdapter(adapter);
+        binding.spFavoriteFilterRating.setAdapter(adapter);
 
-        LinearLayout row_toilet = favorite_fragment.findViewById(R.id.lnl_toilet_favorite_row);
-        row_toilet.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_favoritesFragment_to_toiletDetailsFragment));
+        return binding.getRoot();
+    }
 
-        return favorite_fragment;
+    @Override
+    public void onSelectToilet(View view, Toilet toilet) {
+        Bundle bundle = new Bundle();
+        bundle.putString("toiletId", toilet.getToiletId());
+
+        Navigation.findNavController(view).navigate(R.id.action_favoritesFragment_to_toiletDetailsFragment, bundle);
     }
 }
